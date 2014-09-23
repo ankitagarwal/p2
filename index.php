86f84f7baa8f82a06dc7c9af4830458bc083a12d
Ankit Agarwal <ankit@moodle.com>
2014-09-23 12:55:08 +0530

    Add a very basic index.php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name='keywords' content='ankit agarwal, ankit, portfolio'>
+    <meta name='description' content='This page is a simple portfolio about Ankit Agarwal'>
+    <title>Ankit Agarwal's portfolio</title>
+    <!-- Add bootsrap stuff -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>XKCD style password generator</h1>
+        <p>This is an simple application that generates XKCD style passwords, based on various configurations.</p>
+        <p><img src="http://imgs.xkcd.com/comics/password_strength.png" class="img-responsive img-rounded" alt="xkcd password generator image"></p>
+    </div>
+</body>
+</html>
